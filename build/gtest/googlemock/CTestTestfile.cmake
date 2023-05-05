128c8cc0cf1d3207301f70b1e7475672e07087aa
sheepskins <mitchrosser13@gmail.com>
2023-05-05 12:41:54 +1000

    Initial Commit

---
+++ build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/mitch/catkin_ws/src/instance_segmentation_depth/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
